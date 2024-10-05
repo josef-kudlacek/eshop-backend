86fb4546545739d6256698f2e7b44d595df5520a
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-10-05 13:10:12 +0200

    Initial commit

---
+++ src/main/java/cz/jkdabing/backend/configuration/JasyptConfig.java
@@ -0,0 +1,9 @@
+package cz.jkdabing.backend.configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+}
