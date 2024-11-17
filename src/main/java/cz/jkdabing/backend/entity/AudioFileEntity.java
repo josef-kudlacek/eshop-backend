47b20ecd2be0299c4fc6da57bf6b9f12da64d0bd
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-11-17 15:20:04 +0100

    Change data structure for files and audio files

--- src/main/java/cz/jkdabing/backend/entity/AudioFileEntity.java
+++ src/main/java/cz/jkdabing/backend/entity/AudioFileEntity.java
@@ -3,11 +3,8 @@ package cz.jkdabing.backend.entity;
 import cz.jkdabing.backend.enums.AudioFormatType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.TimeZoneStorage;
-import org.hibernate.annotations.TimeZoneStorageType;
 
 import java.time.LocalTime;
-import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Entity
@@ -21,33 +18,24 @@ public class AudioFileEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID fileId;
+    private UUID audioFileId;
+
+    @OneToOne
+    @JoinColumn(name = "file_id")
+    private FileEntity fileId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private ProductEntity product;
-
-    @Column(nullable = false)
-    private String fileName;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AudioFormatType audioFormatType;
 
     @Column(nullable = false)
-    private String fileUrl;
-
-    @Column(nullable = false)
     private LocalTime length;
 
-    @Column(nullable = false)
-    private Long size;
-
     private Integer bitrate;
-
-    @TimeZoneStorage(TimeZoneStorageType.NORMALIZE)
-    @Column(nullable = false)
-    private ZonedDateTime uploadDate;
 
     @Column(nullable = false)
     private int sequence;
--- src/main/java/cz/jkdabing/backend/entity/FileEntity.java
+++ src/main/java/cz/jkdabing/backend/entity/FileEntity.java
@@ -1,12 +1,10 @@
 package cz.jkdabing.backend.entity;
 
-import cz.jkdabing.backend.enums.AudioFormatType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.TimeZoneStorage;
 import org.hibernate.annotations.TimeZoneStorageType;
 
-import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,27 +30,15 @@ public class FileEntity {
     @Column(nullable = false)
     private String fileName;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private AudioFormatType audioFormatType;
-
     @Column(nullable = false)
     private String fileUrl;
 
     @Column(nullable = false)
-    private LocalTime length;
-
-    @Column(nullable = false)
     private Long size;
-
-    private Integer bitrate;
 
     @TimeZoneStorage(TimeZoneStorageType.NORMALIZE)
     @Column(nullable = false)
     private ZonedDateTime uploadDate;
-
-    @Column(nullable = false)
-    private int sequence;
 
     @OneToMany(mappedBy = "file")
     private List<UserAccessEntity> accessEntityList = new ArrayList<>();
