e64fa817bdefe2b6faa7384cdd1d0fd4e2b884a6
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-12-01 23:11:11 +0100

    Upload file

--- src/main/java/cz/jkdabing/backend/constants/FileConstants.java
+++ src/main/java/cz/jkdabing/backend/constants/FileConstants.java
@@ -8,6 +8,9 @@ public class FileConstants {
     @SuppressWarnings("java:S1075")
     public static final String PRODUCT_AUDIO_FILE_RELATIVE_PATH = "/products/audio/";
 
+    @SuppressWarnings("java:S1075")
+    public static final String PRODUCT_FILE_RELATIVE_PATH = "/products/files/";
+
     private FileConstants() {
 
     }
