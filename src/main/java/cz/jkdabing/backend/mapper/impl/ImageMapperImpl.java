6722a7e5f8aa1cfd37dd3c60b1729d71690f7d64
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-12-08 13:36:05 +0100

    Handle NPE for mapping image

--- src/main/java/cz/jkdabing/backend/mapper/impl/ImageMapperImpl.java
+++ src/main/java/cz/jkdabing/backend/mapper/impl/ImageMapperImpl.java
@@ -3,14 +3,17 @@ package cz.jkdabing.backend.mapper.impl;
 import cz.jkdabing.backend.dto.response.ImageResponse;
 import cz.jkdabing.backend.entity.ImageEntity;
 import cz.jkdabing.backend.mapper.ImageMapper;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ImageMapperImpl implements ImageMapper {
 
     @Override
-    public ImageResponse toResponse(@NotNull ImageEntity imageEntity) {
+    public ImageResponse toResponse(ImageEntity imageEntity) {
+        if (imageEntity == null) {
+            return null;
+        }
+
         String imagePath = imageEntity.getImageUrl() + imageEntity.getImageName();
 
         return ImageResponse.builder()
