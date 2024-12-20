a645b742dc65af6c24f0fb6026f59c0153a8c716
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-12-20 20:28:20 +0100

    Remove item from cart

--- src/main/java/cz/jkdabing/backend/dto/response/CartResponse.java
+++ src/main/java/cz/jkdabing/backend/dto/response/CartResponse.java
@@ -1,5 +1,6 @@
 package cz.jkdabing.backend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import cz.jkdabing.backend.enums.CartStatusType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class CartResponse {
 
     private UUID cartId;
