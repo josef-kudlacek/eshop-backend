1dbb9d7c3607b1af8f7c84d9cf2d63fb35899f06
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-12-29 21:50:34 +0100

    Fix PR #4 comments and remove HelloController with test messages.

--- src/main/java/cz/jkdabing/backend/controller/ProductGenreController.java
+++ src/main/java/cz/jkdabing/backend/controller/ProductGenreController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/productGenre")
+@RequestMapping("/api/product-genre")
 public class ProductGenreController extends AbstractBaseController {
 
     private final ProductGenreService genreProductService;
