47bd2103e6ee2298f29531113e075f4a6c97a6b3
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-12-29 22:42:11 +0100

    Change path to kebab-case

--- src/main/java/cz/jkdabing/backend/controller/AudioFileController.java
+++ src/main/java/cz/jkdabing/backend/controller/AudioFileController.java
@@ -19,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/audioFiles")
+@RequestMapping("/api/audio-files")
 public class AudioFileController extends AbstractBaseController {
 
     private static final Logger logger = LoggerFactory.getLogger(AudioFileController.class);
